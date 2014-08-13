2bbc68b0ee47c4df8b3c06d3adcc3378b3c0862d
Derek Liang <frostyrusty@gmail.com>
2014-08-13 13:18:03 -0700

    added intent usage, new activity webview

--- gen/com/frostyrusty/blogreader/R.java
+++ gen/com/frostyrusty/blogreader/R.java
@@ -1094,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003d;
+        public static final int action_settings=0x7f05003e;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1119,7 +1119,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
-        public static final int progressBar1=0x7f05003c;
+        public static final int progressBar1=0x7f05003d;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1144,6 +1144,7 @@ containing a value of this type.
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int webView1=0x7f05003c;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1196,11 +1197,13 @@ containing a value of this type.
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
-        public static final int activity_main_list=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_blog_web_view=0x7f030018;
+        public static final int activity_main_list=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
-        public static final int main_list=0x7f0d0000;
+        public static final int blog_web_view=0x7f0d0000;
+        public static final int main_list=0x7f0d0001;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1249,6 +1252,7 @@ containing a value of this type.
         public static final int error_title=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
         public static final int no_items=0x7f0a0010;
+        public static final int title_activity_blog_web_view=0x7f0a0013;
     }
     public static final class style {
         /** 
--- src/com/frostyrusty/blogreader/MainListActivity.java
+++ src/com/frostyrusty/blogreader/MainListActivity.java
@@ -17,15 +17,17 @@ import org.json.JSONObject;
 import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -59,7 +61,37 @@ public class MainListActivity extends ListActivity {
 		{
 			Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
 		}
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id)
+	{
+		super.onListItemClick(l, v, position, id);
 		
+		try {
+			JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+			JSONObject jsonPost = jsonPosts.getJSONObject(position);
+			String blogUrl = jsonPost.getString("url");
+			
+			// Opens in external browser:
+			/*
+			Intent intent = new Intent(Intent.ACTION_VIEW);
+			intent.setData(Uri.parse(blogUrl));
+			startActivity(intent);
+			*/
+			
+			// Open within our application (running a new Activity):
+			Intent intent = new Intent(this, BlogWebViewActivity.class);
+			intent.setData(Uri.parse(blogUrl));
+			startActivity(intent);
+			
+		} catch (JSONException e) {
+			logException(e);
+		}
+	}
+
+	private void logException(Exception e) {
+		Log.e(TAG, "Exception caught!", e);
 	}
 
 	private boolean isNetworkAvailable() {
@@ -73,13 +105,6 @@ public class MainListActivity extends ListActivity {
 		}
 		
 		return isAvailable;
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main_list, menu);
-		return true;
 	}
 
 	@Override
@@ -130,7 +155,7 @@ public class MainListActivity extends ListActivity {
 				setListAdapter(adapter);
 				
 			} catch (JSONException e) {
-				Log.e(TAG, "Exception caught!", e);
+				logException(e);
 			}
 		}
 	}
@@ -177,11 +202,11 @@ public class MainListActivity extends ListActivity {
 				}
 				
 			} catch (MalformedURLException e) {
-				Log.e(TAG, "Exception caught: ", e);
+				logException(e);
 			} catch (IOException e) {
-				Log.e(TAG, "Exception caught: ", e);
+				logException(e);
 			} catch (Exception e) {
-				Log.e(TAG, "Exception caught: ", e);
+				logException(e);
 			}
 			
 			return jsonResponse;
